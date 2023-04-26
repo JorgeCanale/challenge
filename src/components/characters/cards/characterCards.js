82642eba61b57631296162dcb4c333c0ba5eea6e
jorge canale <jorgecanale636@yahoo.comm>
2023-04-26 00:20:08 -0300

    css changes

--- src/components/characters/cards/characterCards.js
+++ src/components/characters/cards/characterCards.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React,{useState} from "react";
 import { useSelector } from "react-redux";
 import { CharacterCard } from "../card/characterCard";
+import { Pagination } from "../../pagination/pagination";
 import"./cards.css"
 
 export const CharactersCards =()=>{
 
     const characters = useSelector( state => state.characters.characters)
+    const [page,setPage] = useState(1)
+    const [charactersPerPage ,setCharactersPerPage] = useState(23)
+    const max = Math.ceil(characters.length / charactersPerPage)
 
     return(
-        <div className="cards">
-            {characters && characters.map((character, index) =>{
-            return(
-                <CharacterCard key={index} name={character.name} eyesColor={character.eye_color} gender={character.gender} />
-            )
-        })}
+        <div className="container">
+            <div className="cards">
+             {characters && characters.slice((page-1) * charactersPerPage ,(page-1) * charactersPerPage + charactersPerPage).map((character, index) =>{
+             return(
+                 <CharacterCard key={index} name={character.name} eyesColor={character.eye_color} gender={character.gender} />
+             )
+             })}
+            </div>
+
+            < Pagination  className="Pagination" page={page} setPage={setPage} max={max}/>
         </div>
     )
 }
---
+++ src/components/pagination/pagination.js
@@ -0,0 +1,45 @@
+import React,{useState} from "react";
+import "./pagination.css"
+
+
+export const Pagination  = ({page,setPage,max}) =>{
+
+    const [currentPage, setCurrentPage] = useState(1)
+
+    const nextPage = ()=>{
+        setCurrentPage(currentPage + 1)
+        setPage(page + 1)
+        window.scrollTo(0, 100)
+    }
+    const prevPage=()=>{
+        setCurrentPage(currentPage - 1)
+        setPage(page - 1)
+        window.scrollTo(0, 100)
+    }
+
+    const onKeyDown = e =>{
+        let val = parseInt(e.target.value)
+        if(e.keyCode === 13){
+            setCurrentPage(parseInt(e.target.value))
+            if(isNaN(val) || val < 1 || val > max){
+                setCurrentPage(1)
+                setPage(1)
+            }else{
+                setPage(val)
+                window.scrollTo(0, 100)
+            }
+        }
+    }
+
+    return(
+        <div className="pagination">
+            <button className="previous" onClick={prevPage} disabled={page === 1}>
+                prev
+            </button>
+                <input className="page" value={currentPage} autoComplete="off" onKeyDown={e=>onKeyDown(e)} onChange={e=>setCurrentPage(e.target.value)}/>
+            <button className="next" onClick={nextPage} disabled={ page === max}>
+                next  
+            </button>
+        </div>
+    )
+}
